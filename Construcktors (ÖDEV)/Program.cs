f035178a339000497b17e389dab285a364b69601
Burak Can Kaya <burak.kaya24@ogr.sakarya.edu.tr>
2021-01-22 21:19:08 +0300

    Proje dosyası ekle.

---
+++ Construcktors (ÖDEV)/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Construcktors__ÖDEV_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer { ID = 1, Name = "Burak", SurName = "KAYA", City = "istanbul" };
+            Customer customer2 = new Customer(2, "Can", "Kaya", "İstanbul");
+
+            Console.WriteLine(customer.ID + " " + customer.Name + " " + customer.SurName + " " + customer.City);
+            Console.WriteLine(customer.ID + " " + customer2.Name + " " + customer2.SurName + " " + customer2.City);
+        }
+    }
+    class Customer
+    {
+        public Customer()
+        {
+
+        }
+        //default Construcktor / eklemesen bile çalışır.
+        public Customer(int id , string name, string surName , string city)
+        {
+            //Eğer eşitlemezsen hata verir ! 
+            ID = id;
+            Name = name;
+            SurName = surName;
+            City = city;
+        }
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string City { get; set; }
+    }
+}
